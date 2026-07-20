98e211b46ed1c8c6c6036b23df94d05a48b10c4d
John Ozuna-Diaz <john.ozuna01@gmail.com>
2026-07-20 10:19:00 -0400

    Remove stale duplicate Prototype 2 folder left by the merge, the real project is src/ and data/ at the root

--- Prototype 2/src/MasterProductList.java
+++
@@ -1,9 +0,0 @@
-public class MasterProductList {
-    private String productId;
-    private String description;
-    private String brandName;
-    private String ContainerName;
-    private String productStatus;
-
-
-}
